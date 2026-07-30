50524575b81c7b4e6de088765b0dd805d308a7e9
Sureshkumar Saravanan <sureshkumar18012006@gmail.com>
2026-07-30 22:17:54 +0530

    Create solution.py

---
+++ 125. Valid Palindrom/solution.py
@@ -0,0 +1,60 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         result=""
+#         for ch in s:
+#             if ch.isalnum():
+#                 result+=ch.lower()
+#         if result==result[::-1]:
+#             return True
+#         else:
+#             return False
+        
+
+# Original String
+#       │
+#       ▼
+# Remove special characters
+#       │
+#       ▼
+# Convert to lowercase
+#       │
+#       ▼
+# New String
+#       │
+#       ├── Reverse it
+#       │
+#       ▼
+# Compare both strings
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         cleaned = ""
+
+#         for ch in s:
+#             if ch.isalnum():
+#                 cleaned += ch.lower()
+
+#         return cleaned == cleaned[::-1]
